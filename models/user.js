f0df4a009ddc3874fbcdd587e6b2418c7537d99d
sulbhagarg <sulbhagarg99@gmail.com>
2020-09-27 03:04:26 +0530

    updated model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
     userId:String,
     username:String,
-    picture:String
+    picture:String,
+    firstName:String,
+    lastName:String,
+    addressL1:String,
+    addressL2:String,
+    addressL3:String,
+    pincode:String,
+    contact:String
 });
 
 // export model user with UserSchema
