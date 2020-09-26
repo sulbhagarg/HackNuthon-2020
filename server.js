62ba043c868908b4dc30e4e4e7f67a21522fc56b
sulbhagarg <sulbhagarg99@gmail.com>
2020-09-26 14:47:56 +0530

    connected to database

--- server.js
+++ server.js
@@ -4,17 +4,29 @@
 global.express = require('express');
 global.app = express();
 global.path = require('path');
+global.mongoose = require('mongoose');
 
 // =======================
 // Environment Variables
 // =======================
 var PORT = 3000;
+var URL = "mongodb+srv://sulbha:databasePassword@maindatabase.ykfyn.mongodb.net/Database?retryWrites=true&w=majority"
 
 // ===========================
 // Setting up the view engine
 // ===========================
 app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
+
+// ==============================
+// Connection setup to database
+// ==============================
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    console.log("mongodb is connected");
+}).catch((error)=>{
+    console.log("mongodb not connected");
+    console.log(error);
+});
 
 // ===========
 // Home Route
